e4881950e8880d7325fbb6765d17cf3efdc7e6af
Nelzio <nelziositoe@gmail.com>
2021-05-04 14:50:05 +0200

    Add doc to code

--- src/reference.js
+++ src/reference.js
@@ -1,9 +1,11 @@
 
 function calcSIandPN(pi) {
+    // Pn = (Pi * 10) mod 97
     return (pi * 10) % 97
 }
 
 function checkDigit(pn) {
+    // Check digit = 98 – Pn
     const checkDigit = 98 - pn
     return checkDigit < 10 ? `0${checkDigit}` : checkDigit
 }
@@ -20,11 +22,12 @@ function referenceGeneretor(req, res) {
     for (let index = 0; index < entidadeReferencia.length; index++) {
         // calc si and pi
         const element = entidadeReferencia[index];
-        si = parseInt(element) + pi
-        pi = (si * 10) % 97
+        si = parseInt(element) + pi // Si = digito(i) + Pi
+        pi = (si * 10) % 97 // Pi = (Si * 10) mod 97
     }
     const pn = calcSIandPN(pi)
     const cd = checkDigit(pn)
+    //Referência = Número factura + Mês + Check digit
     const referencia = numeroFactura + mes + cd
     res.json({ si: si, pi: pi, pn: pn, cd: cd, referencia: referencia })
 }
